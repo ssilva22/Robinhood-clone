be69a38a841d9b754d31e845c555889ccf9c0ac9
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-18 15:59:31 -0400

    needs some extra features but basic functionality is uo and running

--- robinhood-lookalike/src/App.js
+++ robinhood-lookalike/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
+import NewsFeed from "./Newsfeed";
+import Stats from "./Stats";
 
 function App() {
   return (
-    <div className="App">
-      {/*Header */}
+    <div className="app">
       <div className="app__header">
-      <Header/>
+        <Header />
       </div>
       <div className="app__body">
-
+        <div className="app__container">
+          <NewsFeed />
+          <Stats />
+        </div>
       </div>
     </div>
   );
