3088dd060e2bae39bf5202f175141786b750aff1
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-18 13:51:57 -0400

    react app created

---
+++ robinhood-lookalike/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/*Header */}
+      <div className="app__header">
+      <Header/>
+      </div>
+      <div className="app__body">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ robinhood-lookalike/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+    <div className="header__wrapper">
+    {/* logo*/}
+    <div className="header__logo">
+        
+    </div>
+    {/* search*/}
+    {/* menuItems*/}
+    </div>
+    )
+}
+
+export default Header;
